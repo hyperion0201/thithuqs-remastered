d933d7c7b8741af8ee61f66441ae3aefbc840bb0
Hieu Hoang <megamind.ft@gmail.com>
2018-07-18 17:41:00 +0700

    integrate sidebar to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SidebarExample from './components/SidebarExample';
 
 class App extends Component {
   render() {
@@ -8,11 +9,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Chào mừng bạn đến với Thi Thử Quân Sự</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Để bắt đầu thi, hãy chọn học phần:
         </p>
+        <p className="About">
+        Click vào nút ở góc trái để chọn học phần (có 3 gói câu hỏi của 3 học phần)</p>
+      <SidebarExample className= "Sidebar"/>
       </div>
     );
   }
